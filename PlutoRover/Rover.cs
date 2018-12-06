c74251ccacd39a2545a38ad093d7e06027330fa3
Peter de Klerk <peterdeklerk@gmail.com>
2018-12-06 08:49:29 +0000

    Update to add the first ProcessCommand iteration! It will process F or B from the starting position. Also, an enum was defined for the directions to make it more readable, and 2 constants were added to define the grid size

--- PlutoRover/Rover.cs
+++ PlutoRover/Rover.cs
@@ -18,6 +18,10 @@ namespace PlutoRover
         public int currentY { get; set; }
         public DIRECTION currenctDirection { get; set; }
 
+        // define the limits of the grid representing Pluto (10x10)
+        private const int MaxX = 9;
+        private const int MaxY = 9;
+
         public Rover()
         {
             currentX = 0;
@@ -25,7 +29,28 @@ namespace PlutoRover
             currenctDirection = DIRECTION.N;
         }
 
+        // process to accept a command character and act accordingly (F, B, L, R)
+        public void ProcessCommand(char command)
+        {
+            // Process commands for F and B, work out how much to move by
+            var moveby = command == 'F' ? 1 : command == 'B' ? -1 : 0;
 
+            // if N or S, then Y changes, if E or W, then X changes
+            int newX = currentX;
+            int newY = currentY + moveby;
+
+            // make sure we wrap around the grid (IE if we reach the edge, go to the other side)
+            newX = newX == -1 ? newX = MaxX : newX > MaxX ? 0 : newX;
+            newY = newY == -1 ? newY = MaxY : newY > MaxY ? 0 : newY;
+
+            // move the rover
+            currentX = newX;
+            currentY = newY;
+
+
+
+
+        }
 
     }
 }
