0cea74926bc9f58d9bbf3fc6db19682f2492bc1e
Peter de Klerk <peterdeklerk@gmail.com>
2018-12-06 08:42:53 +0000

    Include new tests to move F and B from the starting position is correct

--- PlutoRover.Test/RoverTests.cs
+++ PlutoRover.Test/RoverTests.cs
@@ -15,5 +15,35 @@ namespace PlutoRover.Test
             // Assert
             Assert.AreEqual("0,0,N", string.Format("{0},{1},{2}", rover.currentX, rover.currentY, rover.currenctDirection));
         }
+
+        // tests to ensure F and B from starting position is correct (going to work with a grid 10x10)
+        [Test]
+        public void Rover_Press_F_FromStartingPosition_Return01N()
+        {
+            // Arrange
+            var rover = new Rover();
+
+            // Act
+            rover.ProcessCommand('F');
+
+            // Assert
+            Assert.AreEqual("0,1,N", string.Format("{0},{1},{2}", rover.currentX, rover.currentY, rover.currenctDirection));
+        }
+
+        [Test]
+        public void Rover_Press_B_FromStartingPosition_Return09N()
+        {
+            // Arrange
+            var rover = new Rover();
+
+            // Act
+            rover.ProcessCommand('B');
+
+            // Assert
+            Assert.AreEqual("0,9,N", string.Format("{0},{1},{2}", rover.currentX, rover.currentY, rover.currenctDirection));
+        }
+
+
+
     }
 }
