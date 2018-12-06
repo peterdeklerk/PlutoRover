20a703e8d4a6124384512b4b363fd62ae204e0eb
Peter de Klerk <peterdeklerk@gmail.com>
2018-12-06 08:30:24 +0000

    Update to add the unit test project for the pluto rover using NUnit, with the first test method

---
+++ PlutoRover.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using PlutoRover;
+
+namespace PlutoRover.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Rover_NewRoverStartingPosition_Returns00N()
+        {
+
+
+
+
+
+
+        }
+    }
+}
